721b06f2e38ba694b9f2778cf0d0f932682e3108
jackie deng <63827088@qq.com>
2020-10-08 13:12:41 +0800

    dev

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,14 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.cluster = {
+        listen: {
+            path: '',
+            port: 80,
+            hostname: '127.0.0.1',
+        }
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
